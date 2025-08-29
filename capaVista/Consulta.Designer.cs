86d10aa6e99cf73439caf61b4bf342963bb51083
CesarEduardoSantizo <santizosantosf37@gmail.com>
2025-08-29 17:47:13 -0600

    Capacitacion

---
+++ capaVista/Consulta.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace capaVista
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Dgv_registros = new System.Windows.Forms.DataGridView();
+            this.btn_Consultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_registros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dgv_registros
+            // 
+            this.Dgv_registros.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.Dgv_registros.AllowUserToAddRows = false;
+            this.Dgv_registros.AllowUserToDeleteRows = false;
+            this.Dgv_registros.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Dgv_registros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_registros.Location = new System.Drawing.Point(24, 162);
+            this.Dgv_registros.Name = "Dgv_registros";
+            this.Dgv_registros.ReadOnly = true;
+            this.Dgv_registros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Dgv_registros.Size = new System.Drawing.Size(751, 276);
+            this.Dgv_registros.TabIndex = 0;
+            // 
+            // btn_Consultar
+            // 
+            this.btn_Consultar.Location = new System.Drawing.Point(657, 61);
+            this.btn_Consultar.Name = "btn_Consultar";
+            this.btn_Consultar.Size = new System.Drawing.Size(93, 63);
+            this.btn_Consultar.TabIndex = 1;
+            this.btn_Consultar.Text = "Consultar";
+            this.btn_Consultar.UseVisualStyleBackColor = true;
+            // 
+            // Consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Consultar);
+            this.Controls.Add(this.Dgv_registros);
+            this.Name = "Consulta";
+            this.Text = "Consulta";
+            this.Load += new System.EventHandler(this.Consulta_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_registros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Dgv_registros;
+        private System.Windows.Forms.Button btn_Consultar;
+    }
+}
